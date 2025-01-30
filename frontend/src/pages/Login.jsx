bc2c6e47b878ef2fb6dbce8984a9097a8c8743ea
VipulVW14 <vipulwasnik0@gmail.com>
2025-01-30 19:44:08 +0530

    fix: tailwind not applied error

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,27 +17,68 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <form onSubmit={handleLogin} className="bg-white p-6 rounded shadow-md">
-        <h2>Login to your account</h2>
-        <input
-          type="email"
-          placeholder="Email"
-          className="block border p-2"
-          onChange={(e) => setEmail(e.target.value)}
+    <div className="grid grid-cols-4 bg-white h-screen w-full">
+      <div className="w-full items-center flex flex-col mt-15 rounded-lg">
+        <p className="text-3xl mb-15 font-mono font-bold leading-tight tracking-normal text-gray-800">Task Management App</p>
+        <p className="text-2xl mb-9 font-mono">
+            Login to your account
+        </p>
+        
+        <form onSubmit={handleLogin} className="bg-white w-70 mx-10">
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-[20px] font-medium text-gray-900"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              className="border-b-2 rounded-sm border-gray-300 text-gray-900 sm:text-sm focus:outline-none focus:border-b-2-blue-600 block w-full p-2.5"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          
+          <br/>
+
+          <div>
+            <label
+              htmlFor="password"
+              className="block mb-2 text-[20px] font-medium text-gray-900"
+            >
+              Password
+            </label>
+
+            <input
+              type="password"
+              name="Password"
+              id="password"
+              placeholder="Enter your password"
+              className="border-b-2 rounded-sm border-gray-300 text-gray-900 sm:text-sm focus:outline-none focus:border-b-2-primary-600 block w-full p-2.5"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <br/>
+
+          <button
+            type="submit"
+            className="text-gray-100 bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-sm text-md w-full mt-3 px-5 py-2 text-center"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+      
+      <div className="col-span-3">
+        <img
+          className=" w-full h-full"
+          src="https://i.postimg.cc/1zMrsJLZ/DALL-E-2025-01-30-17-05-02-A-modern-and-sleek-task-management-app-interface-displayed-on-a-futuris.webp"
         />
-        <br/>
-        <input
-          type="password"
-          placeholder="Password"
-          className="block border p-2 mt-2"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br className="mb-10"/>
-        <button type="submit" className="bg-blue-500 text-white px-4 py-2 mt-12">
-          Login
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -35,7 +35,7 @@ const Tasks = () => {
   return (
     <div className="p-6">
       <h2 className="text-2xl font-bold mb-4">Tasks</h2>
-      <button onClick={() => setIsModalOpen(true)} className="bg-green-500 text-white px-4 py-2 mb-4">
+      <button onClick={() => setIsModalOpen(true)} className="bg-green-500 text-white px-4 py-2 mb-4 rounded-md">
         + Add Task
       </button>
 
@@ -43,11 +43,13 @@ const Tasks = () => {
         {tasks.map((task) => (
           <li key={task.id} className="border p-2 mt-2 flex justify-between">
             <span>{task.title}</span>
+            <br></br>
+            <span>{task.description}</span>
             <div>
-              <button onClick={() => handleEdit(task)} className="bg-yellow-500 text-white px-4 py-1 mx-2">
+              <button onClick={() => handleEdit(task)} className="bg-yellow-500  rounded-md text-white px-4 py-1 mx-2">
                 Edit
               </button>
-              <button onClick={() => handleDelete(task.id)} className="bg-red-500 text-white px-4 py-1">
+              <button onClick={() => handleDelete(task.id)} className="bg-red-500  rounded-md text-white px-4 py-1">
                 Delete
               </button>
             </div>
